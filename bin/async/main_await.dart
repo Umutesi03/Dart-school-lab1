bbd560a0c0a5bf76904e445e3581c8f5d60751cf
Fortunee SHIMWAMUBYEYI <shimwamubyeyifor2003@gmail.com>
2026-02-06 15:40:01 -0800

    Update main_await

--- bin/async/main_await.dart
+++ bin/async/main_await.dart
@@ -3,19 +3,15 @@ import '../collections/list.dart';
 Future<List<Student>> loadStudents() async {
   print('Loading students...');
 
-
-  await Future.delayed(Duration(seconds: 2));
+  // Simulate a delay like fetching data from a database or API
+  await Future.delayed(const Duration(seconds: 2));
 
   print('Students loaded!');
   return students;
 }
 
-
 void main() async {
-
-  List<Student> loadedStudents = await loadStudents();
-
+  final List<Student> loadedStudents = await loadStudents();
 
   print('Total number of students loaded: ${loadedStudents.length}');
 }
-
