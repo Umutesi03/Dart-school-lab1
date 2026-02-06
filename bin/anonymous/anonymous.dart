25c44949d13c47cac3f375494560937fa87525e4
moise744 <habimanamoise77@gmail.com>
2026-02-06 17:58:42 +0200

    Print all student names using an anonymous function

--- bin/anonymous/anonymous.dart
+++ bin/anonymous/anonymous.dart
@@ -1,10 +1,10 @@
-
 import '../collections/list.dart';
 
 void main() {
-print('=== Student Names (Anonymous Function) ===');
+  print('=== Student Names (Anonymous Function) ===');
 
-students.forEach((student) {
-print(student.name);
-});
+  // Using an anonymous function to print each student's name
+  students.forEach((student) {
+    print(student.name);
+  });
 }
