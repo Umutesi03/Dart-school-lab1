5375108fcba1da0844cb6b74fd332f18887a839c
UWIHANGANYEObed <uwihanganyeobed@gmail.com>
2026-02-06 17:35:58 +0200

    Implement Registrable interface and improve Student course registration

--- bin/p4_interfaces/registable.dart
+++ bin/p4_interfaces/registable.dart
@@ -1,5 +1,6 @@
-
-
+/// Question 8:
+/// Registrable is an abstract class (acts like an interface).
+/// Any class that implements Registrable MUST define registerCourse().
 abstract class Registrable {
-void registerCourse(String courseName);
+  void registerCourse(String courseName);
 }
--- bin/p4_interfaces/student.dart
+++ bin/p4_interfaces/student.dart
@@ -1,39 +1,53 @@
+
 import 'package:ansicolor/ansicolor.dart';
-import 'registable.dart'; 
-
+import 'registrable.dart';
+/// Q9:
+/// Student implements the Registrable interface.
+/// This enforces the rule that every Student must
+/// provide an implementation of registerCourse().
 class Student implements Registrable {
-String name;
-int age;
-Student(this.name, this.age);
+  String name;
+  int age;
 
-@override
-void registerCourse(String courseName) {
-print('Student $name has registered for course: $courseName');
+  Student(this.name, this.age);
+
+  /// Implementation of the interface method
+  @override
+  void registerCourse(String courseName) {
+    print('Student $name has registered for the course: $courseName');
+  }
+
+  /// Displays basic student information
+  void displayInfo() {
+    print('Name: $name, Age: $age');
+  }
 }
-void displayInfo() {
-print('Name: $name, Age: $age');
-}
-}
+
 void main() {
-ansiColorDisabled = false;
-AnsiPen greenBold = AnsiPen()..green(bold: true);
-AnsiPen yellowBold = AnsiPen()..yellow(bold: true);
+  // Enable ANSI colors
+  ansiColorDisabled = false;
 
+  // Color styles
+  final greenBold = AnsiPen()..green(bold: true);
+  final yellowBold = AnsiPen()..yellow(bold: true);
 
-Student student1 = Student('Habimana Moise', 20);
-print(yellowBold('Student 1:'));
-student1.displayInfo();
-student1.registerCourse(greenBold('Mobile Application Development'));
-print('');
+  // Student 1
+  final student1 = Student('Habimana Moise', 20);
+  print(yellowBold('Student 1:'));
+  student1.displayInfo();
+  student1.registerCourse(greenBold('Mobile Application Development'));
+  print('');
 
-Student student2 = Student('Umutesi Kelia', 23);
-print(yellowBold('Student 2:'));
-student2.displayInfo();
-student2.registerCourse(greenBold('Database Management Systems'));
-print('');
+  // Student 2
+  final student2 = Student('Umutesi Kelia', 23);
+  print(yellowBold('Student 2:'));
+  student2.displayInfo();
+  student2.registerCourse(greenBold('Database Management Systems'));
+  print('');
 
-Student student3 = Student('Shimwamubyeyi Fortune', 21);
-print(yellowBold('Student 3:'));
-student3.displayInfo();
-student3.registerCourse(greenBold('Web Development'));
+  // Student 3
+  final student3 = Student('Shimwamubyeyi Fortune', 21);
+  print(yellowBold('Student 3:'));
+  student3.displayInfo();
+  student3.registerCourse(greenBold('Web Development'));
 }
