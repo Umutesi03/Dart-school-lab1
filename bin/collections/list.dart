e26291a58462d0314d740742b6b1e9ee69ae69f7
moise744 <habimanamoise77@gmail.com>
2026-02-06 17:54:01 +0200

    Create a List of Student objects and print their details

--- bin/collections/list.dart
+++ bin/collections/list.dart
@@ -1,20 +1,23 @@
-
 class Student {
-int id;
-String name;
-int age;
+  int id;
+  String name;
+  int age;
 
-Student(this.id, this.name, this.age);
+  Student(this.id, this.name, this.age);
 }
 
+// Creating a List of Student objects
 List<Student> students = [
-Student(1, 'Habimana Moise', 20),
-Student(2, 'Umutesi Kelia', 23),
-Student(3, 'Shimwamubyeyi Fortune', 21),
+  Student(1, 'Habimana Moise', 20),
+  Student(2, 'Umutesi Kelia', 23),
+  Student(3, 'Shimwamubyeyi Fortune', 21),
 ];
+
 void main() {
-print('\n === Student List === \n');
-for (var student in students) {
-print('ID: ${student.id}, Name: ${student.name}, Age: ${student.age}');
-}
+  print('\n === Student List === \n');
+  
+  // Iterating over the List and printing each student's details
+  for (var student in students) {
+    print('ID: ${student.id}, Name: ${student.name}, Age: ${student.age}');
+  }
 }
