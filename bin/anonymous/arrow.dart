d96f30fd5f5ee7bcd5848acc4bd95cae220d6de8
moise744 <habimanamoise77@gmail.com>
2026-02-06 17:59:33 +0200

    Add arrow function to greet a student by name

--- bin/anonymous/arrow.dart
+++ bin/anonymous/arrow.dart
@@ -1,5 +1,7 @@
 import 'package:dart_lab1/functions/greeting.dart';
+
 void main() {
+  // Arrow function that prints a greeting for a student
   void greetStudent(String name) => print('${getGreeting()} $name, welcome!');
 
   greetStudent('Umutesi');
